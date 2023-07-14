9d9c15f17d2d49245758cfec76233e48fec90829
IhorPashkov <igorpash96@gmail.com>
2023-07-14 18:47:59 +0300

    registration toolbar view

--- app/src/main/java/com/example/imagespace/ui/onboarding/registration/RegistrationFragment.kt
+++ app/src/main/java/com/example/imagespace/ui/onboarding/registration/RegistrationFragment.kt
@@ -1,7 +1,9 @@
 package com.example.imagespace.ui.onboarding.registration
 
+import android.widget.EditText
 import com.example.imagespace.components.ToolbarListener
 import com.example.imagespace.databinding.FragmentRegistrationBinding
+import com.example.imagespace.extensions.toast
 import com.example.imagespace.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,10 +15,18 @@ class RegistrationFragment :
 
     override fun initViews() {
         toolbar.showToolbar()
+
+        binding.vFocus.requestFocus()
     }
 
     override fun initListeners() {
         toolbar.setOnBackClickListener { goBack() }
+
+        activity?.currentFocus?.let {
+            if (it !is EditText) {
+                toast("Click")
+            }
+        }
     }
 
     override fun onDestroy() {
