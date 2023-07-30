31eefdf6e91ace6b4d4b1c96df6407f680108985
Cascad <slashdrop@gmail.com>
2023-07-30 04:12:42 +0300

    improved bottom navigation view

--- app/src/main/java/com/example/imagespace/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/imagespace/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.imagespace.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.fragment.NavHostFragment
 import com.example.imagespace.R
 import com.example.imagespace.databinding.ActivityMainBinding
+import com.example.imagespace.extensions.toast
 import com.example.imagespace.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,5 +21,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         val navController = navHostFragment.navController
 
         binding.navView.setupWithNavController(navController)
+    }
+
+    fun onUploadPhotoClick(item: MenuItem) {
+        toast("${item.title}")
     }
 }
