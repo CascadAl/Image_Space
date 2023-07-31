ac38360225a6f7884f2f73f094063acbb355f003
IhorPashkov <igorpash96@gmail.com>
2023-07-31 19:32:15 +0300

    added photo api

---
+++ app/src/main/java/com/example/imagespace/components/userinfo/UserInfoData.kt
@@ -0,0 +1,7 @@
+package com.example.imagespace.components.userinfo
+
+data class UserInfoData(
+    val image: String,
+    val name: String,
+    val userName: String
+)
---
+++ app/src/main/java/com/example/imagespace/components/userinfo/UserInfoView.kt
@@ -0,0 +1,17 @@
+package com.example.imagespace.components.userinfo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.imagespace.databinding.ViewUserInfoBinding
+
+class UserInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding: ViewUserInfoBinding =
+        ViewUserInfoBinding.inflate(LayoutInflater.from(context), this, true)
+}
